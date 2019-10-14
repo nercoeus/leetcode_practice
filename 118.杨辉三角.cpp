e5d23c606d854672a7a65cd192412126af48c080
lizhiwei <18252116593@163.com>
2019-10-14 18:27:59 +0800

    ac:118,119

--- 118.杨辉三角.cpp
+++ 118.杨辉三角.cpp
@@ -5,11 +5,26 @@
  */
 
 // @lc code=start
-class Solution {
+#include <vector>
+using namespace std;
+class Solution
+{
 public:
-    vector<vector<int>> generate(int numRows) {
-        
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> res;
+        for (int i = 0; i < numRows; i++)
+        {
+            vector<int> t(i+1);
+            t[0] = 1;
+            t[i] = 1;
+            for (int j = 1; j <= i/2; j++)
+            {
+                t[j] = t[i-j] = res[i-1][j-1] + res[i-1][j];
+            }
+            res.insert(res.end(), t);
+        }
+        return res;
     }
 };
 // @lc code=end
-
