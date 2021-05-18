cb920002906ebff2b8bbac5728dc4f25e9ca22ac
nercoeus <li.zhi.wei.li@qq.com>
2021-05-18 23:16:01 +0800

    ac:1

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -3,32 +3,27 @@
  *
  * [1] 两数之和
  */
-#include<iostream>
-#include<vector>
-using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    int n = nums.size();
-    vector<int> result;
-    for(int i = 0; i < n-1; i++){
-        for(int j = i + 1; j < n; j++){
-            if(nums[i] + nums[j] == target){
-                result.push_back(i);
-                result.push_back(j);
-                break;
+        int n = nums.size();
+        for(int i = 0;i<n;i++) {
+            for(int j = i+1;j<n;j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
             }
         }
+        return {};
     }
-    return result;
-}
+    vector<int> twoSumUseMap(vector<int>& nums, int target) {
+        map<int, int> temp;
+        for(int i = 0;i<nums.size();i++) {
+            if(temp.find(target - nums[i]) != temp.end()) {
+                return {i, temp[target - nums[i]]};
+            }
+            temp[nums[i]] = i;
+        }
+        return {};
+    }
 };
-int main(){
-    Solution* s = new Solution();
-    vector<int> v = {7, 5, 16, 8};
-    vector<int> res = s->twoSum(v, 13);
-    for(int n : res) {
-        std::cout << n << '\n';
-    }
-    return 0;
-}
