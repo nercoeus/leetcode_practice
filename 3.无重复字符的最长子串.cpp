5181467cc80b417137ff32133460ad58f8fad365
nercoeus <li.zhi.wei.li@qq.com>
2021-05-19 23:54:07 +0800

    ac:3

--- 3.无重复字符的最长子串.cpp
+++ 3.无重复字符的最长子串.cpp
@@ -9,16 +9,34 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int> dict(256, -1);
+        int low = -1;
         int res = 0;
-        int s2 = -1;
-        for(int i=  0;i<s.size();i++){
-            if(dict[s[i]] > s2){
-                s2 = dict[s[i]];
+        for(int i = 0;i<s.size();i++) {
+            if(dict[s[i]] > low) {
+                low = dict[s[i]];
             }
             dict[s[i]] = i;
-            res = max(res, i-s2);
+            res = max(res, i-low);
         }
         return res;
+    }
+    int lengthOfLongestSubstring(string s) {
+        int size = s.size();
+        int low = 0;
+        int max = 0;
+        for(int i = 0;i<size;i++) {
+            int j = 0;
+            for(j = low; j < i;j++) {
+                if (s[j] == s[i]) {
+                    low = j+1;
+                    break;
+                }
+            }
+            if(i-low+1 > max) {
+                max = i-low+1;
+            }
+        }
+        return max;
     }
 };
 // @lc code=end
