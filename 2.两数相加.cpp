bb866383355e072a716d21b6db37ce78c6a65e0b
nercoeus <li.zhi.wei.li@qq.com>
2021-05-19 22:47:16 +0800

    ac:2

--- 2.两数相加.cpp
+++ 2.两数相加.cpp
@@ -1,50 +1,79 @@
-/*
- * @lc app=leetcode.cn id=2 lang=cpp
- *
- * [2] 两数相加
- */
-
-// @lc code=start
 /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int d = 0;
-        ListNode* res = l1;
-        while(l1 != NULL && l2 != NULL){
-            d = d + l1->val + l2->val;
-            l1->val = d%10;
-            d = d/10;
-            if(l1->next == NULL && d != 0){
-                l1->next = new ListNode(d);
-                d = 0;
-                swap(l1->next, l2->next);
+        ListNode* head = new ListNode();
+        ListNode* pr = head;
+        int value = 0;
+        ListNode* temp = new ListNode(0);
+        while(l1 != nullptr || l2 != nullptr) {
+            if (l1 == nullptr) {
+                l1 = temp;
             }
-            if(l1->next == NULL){
-                swap(l1->next, l2->next);
+            if (l2 == nullptr) {
+                l2 = temp;
             }
+            value = (l1->val + l2->val + value);
+            ListNode* next = new ListNode(value%10);
+            value /= 10;
+            pr->next = next;
+            pr = pr->next;
             l1 = l1->next;
             l2 = l2->next;
         }
-        while(l1!=NULL){
-            d = d + l1->val;
-            l1->val = d%10;
-            d = d/10;
-            if(l1->next == NULL && d != 0){
-                l1->next = new ListNode(d);
-                d = 0;
-            }
-            l1 = l1->next;
+        if(value != 0) {
+            ListNode* next = new ListNode(value);
+            pr->next = next;
+            pr = next;
         }
-        return res;
+        return head->next;
+    }
+
+    ListNode* addTwoNumbersBk(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode();
+        ListNode* pr = head;
+        int value = 0;
+        while(l1 != nullptr || l2 != nullptr) {
+            if (l1 == nullptr) {
+                value = (l2->val + value);
+                l2->val = value%10;
+                value /= 10;
+                pr->next = l2;
+                pr = pr->next;
+                l2 = l2->next;
+                continue;
+            }
+            if (l2 == nullptr) {
+                value = (l1->val + value);
+                l1->val = value%10;
+                value /= 10;
+                pr->next = l1;
+                pr = pr->next;
+                l1 = l1->next;
+                continue;
+            }
+            value = (l1->val + l2->val + value);
+            l2->val = value%10;
+            value /= 10;
+            pr->next = l2;
+            pr = pr->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        if(value != 0) {
+            ListNode* next = new ListNode(value);
+            pr->next = next;
+            pr = next;
+        }
+        return head->next;
     }
 };
-// @lc code=end
-
